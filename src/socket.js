e6864c6057ca9b244dbd314abb0e20612f7f33bb
fadel <fadel9wahab@gmail.com>
2025-08-26 16:19:07 +0300

    Use process.env.PORT for deployment

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,10 @@
-// src/socket.js
+// socket.js
 import { io } from "socket.io-client";
-const socket = io("http://localhost:5000");
+
+// ✅ Connect to backend (force WebSocket)
+const socket = io("https://socialnetwork-backend-production-7e1a.up.railway.app", {
+  transports: ["websocket"], 
+  withCredentials: true
+});
+
 export default socket;
